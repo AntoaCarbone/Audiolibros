b1189725ce23f7fb76476ba75f23e8708647c28b
AntoaCarbone <antonella.a.carbone@gmail.com>
2023-10-03 23:29:47 -0300

    Agregue tan 1 y 2

--- AudioLibros/src/app/app-routing.module.ts
+++ AudioLibros/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'audio',
+    loadChildren: () => import('./audio/audio.module').then( m => m.AudioPageModule)
   }
+
 ];
 @NgModule({
   imports: [
